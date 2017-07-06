b3e2d87b0cfd443a6828a7f4aec6551ad8e3493a
Igor <drazovic-92@hotmail.com>
2017-07-06 16:02:30 +0200

    auth0 api tokens

--- src/app/newevent/newevent.component.ts
+++ src/app/newevent/newevent.component.ts
@@ -54,6 +54,7 @@ export class NewEventComponent {
     }
 
     createEvent(eventName) {
+        // Create new event and push iz to firebase
         const town = this.selectedtown;
         const place = this.selectedPlace;
         const sport = this.selectedSport;
@@ -72,6 +73,7 @@ export class NewEventComponent {
         const events = this.db.list('/events', { preserveSnapshot: true });
         events.push(newEvent).once('value', (snapshot) => this.newEventKey = snapshot.key);
 
+        // Create player (owner) of event and push it to firebase
         const notified = true;
         const nickname = this.auth.profile.nickname;
         const picture = this.auth.profile.picture;
@@ -82,6 +84,53 @@ export class NewEventComponent {
         }
         const players = this.db.list('/events/' + this.newEventKey + '/players');
         players.push(newPlayer);
+
+        // Get all user id's from auth0 user database
+        getToken(userid) {
+            const request = require('request');
+            const options = {
+                method: 'POST',
+                url: 'https://drazovic.eu.auth0.com/oauth/token',
+                headers: { 'content-type': 'application/json' },
+                body:
+                {
+                    grant_type: 'client_credentials',
+                    client_id: 'ypS8zEospMg9VhMpezpB50C5lgAg2iKi',
+                    client_secret: 'VoIpRUCsPXPagbaXUOCfPTVnKo3XwaUTplGBee07h6h8106iuEVTkRDhDNGKKrKV',
+                    audience: 'https://drazovic.eu.auth0.com/api/v2/'
+                },
+                json: true
+            };
+            const self = this;
+            request(options, function (error, response, body) {
+                if (error) { throw new Error(error); }
+                self.access_token = body.access_token;
+                self.getUser(userid);
+            });
+        }
+
+        getUser(userid) {
+            const request = require('request');
+            const options = {
+                method: 'GET',
+                url: 'https://matchapp.eu.auth0.com/api/v2/users',
+                headers:
+                {
+                    authorization: 'Bearer ' + this.access_token,
+                    'content-type': 'application/json'
+                },
+                json: true
+            };
+            const self = this;
+            request(options, function (error, response, body) {
+                if (error) { throw new Error(error); }
+                self.user = body;
+                console.log(self.user);
+            });
+        }
+
+        // Create notification for all users that the event is created and push it to firebase
+
     }
 
     alert() {
