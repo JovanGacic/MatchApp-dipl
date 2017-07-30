1150c7841a35f5f67e00fda89fe98fb8a61ef1a3
Jovan <bonjovi1415@hotmail.com>
2017-07-30 20:13:13 +0200

    commit

---
+++ src/API/routes/api.js
@@ -0,0 +1,12 @@
+
+//Dependecies
+var express = require('express');
+var router = express.Router();
+
+//Routes
+router.get('/products', function(req, res){
+    res.send('api is working')
+});
+
+//Return router
+module.exports = router;
